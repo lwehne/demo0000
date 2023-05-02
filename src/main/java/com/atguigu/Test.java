a455ff22e1652717fb2b95f23c6086e730e622ea
why <why@git.com>
2023-05-03 02:16:12 +0800

    third commit

--- src/main/java/com/atguigu/Test.java
+++ src/main/java/com/atguigu/Test.java
@@ -2,7 +2,7 @@ package com.atguigu;
 
 public class Test {
     public static void main(String[] args) {
-//  （1）1.创建一个Customer ，名字叫 Jane Smith；66
+//  （1）1.创建一个Customer ，名字叫 Jane Smith；663
         Customer customer = new Customer("Smith", "Jane");
 //      2.他有一个账号为1000，余额为2000元，年利率为 1.23％ 的账户。
         Account account = new Account(1000, 2000, 0.0123);
